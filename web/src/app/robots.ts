b30c1a0069cd258f5c99a6cb90347f5eace1ef75
Suraj Thakuri <thakurisuraj38@gmail.com>
2026-04-28 01:54:11 +1000

    Fix: Add missing MetadataRoute import in robots.ts

--- web/src/app/robots.ts
+++ web/src/app/robots.ts
@@ -1,5 +1,5 @@
 import { client } from '@/lib/sanity'
-
+import { MetadataRoute } from 'next'
 export default async function robots(): Promise<MetadataRoute.Robots> {
   const businessMetaData = await client.fetch(`*[_type == "businessMetaData"][0]{ businessUrl }`);
   const domain = businessMetaData?.businessUrl || 'https://undefined_setmetadata_in_studio.com';
